09b2af123f55c97d54d58ee1581db1dc14c3eac2
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-25 00:08:31 -0400

    Phase 3 building nested hashes code along part 2

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,8 +21,9 @@ def update_status
    }
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
-
+  # Code your solution here
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
 
@@ -59,8 +60,10 @@ def add_characters
    #   - The first hash should have the :name key set to "Prince Escalus" 
    #   - The second hash should have the :name key set to "Apothecary" 
    # Code your solution here:
-
-
+   epic_tragedy[:additional_characters] = []
+   epic_tragedy[:additional_characters][0] = { name: "Prince Escalus" }
+   epic_tragedy[:additional_characters][1] = { name: "Apothecary" } 
+ 
 
 
 
